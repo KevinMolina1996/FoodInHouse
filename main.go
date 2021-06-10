0a43b92e5bf6b8853d1ff7ab52123c59691ee13b
Joan Sebastian Castañeda Correa <jcasco94@gmail.com>
2021-06-09 23:02:02 -0500

    [FEATURE] Add listener on server

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Wellcome to FoodInHouse API")
+}
 
 func main() {
-	fmt.Println("Hello Word")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+	fmt.Println("Server run on localhost:3000")
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
